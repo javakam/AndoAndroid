dcbbf6d4162eb01dbd1590c869c26121c48a6015
changbao <jooybao@foxmail.com>
2018-06-28 17:21:02 +0800

    更新README

---
+++ app/src/main/java/com/ishiqing/modules/incremental_updating/增量更新.java
@@ -0,0 +1,22 @@
+package com.ishiqing.modules.incremental_updating;
+
+import com.ishiqing.base.BaseFragment;
+
+/**
+ * Android 增量更新 -- 2H
+ * <p>
+ * https://www.cniao5.com/course/courseware/10054
+ * <p>
+ * Created by javakam on 2018/6/28.
+ */
+public class 增量更新 extends BaseFragment {
+    @Override
+    protected int getLayoutResId() {
+        return 0;
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+}
