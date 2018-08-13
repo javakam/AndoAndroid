893a73a6e3fbe09bab6b99f3d4bc258b23cf6ef4
changbao <jooybao@foxmail.com>
2018-08-13 20:06:08 +0800

    Android多线程-AsykTask

--- app/src/main/java/com/ishiqing/modules/thread/AsyncTaskFragment.java
+++ app/src/main/java/com/ishiqing/modules/thread/AsyncTaskFragment.java
@@ -1,10 +1,15 @@
 package com.ishiqing.modules.thread;
 
+import android.app.Activity;
 import android.os.AsyncTask;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.view.View;
 
 import com.ishiqing.R;
 import com.ishiqing.base.BaseFragment;
+
+import java.lang.ref.WeakReference;
 
 /**
  * AsyncTask
@@ -20,27 +25,53 @@ public class AsyncTaskFragment extends BaseFragment {
     @Override
     protected void initViews(View v) {
         initTopBar("AsyncTask", true);
+
+        new MyAsyncTask(mActivity, "MyAsyncTask#1  ").execute("123");
+        new MyAsyncTask(mActivity, "MyAsyncTask#2  ").execute("123");
+        new MyAsyncTask(mActivity, "MyAsyncTask#3  ").execute("123");
     }
 
-    class MyAsyncTask extends AsyncTask<Integer,String,String>{
+    class MyAsyncTask extends AsyncTask<String, Integer, String> {
+        WeakReference<Activity> mWeakActivity;
+        private String mName;
+
+        public MyAsyncTask(Activity activity, String mName) {
+            mWeakActivity = new WeakReference<Activity>(activity);
+            this.mName = mName;
+        }
+
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
         }
 
         @Override
-        protected String doInBackground(Integer... integers) {
-            return null;
+        protected String doInBackground(String... strings) {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return mName;
         }
 
         @Override
-        protected void onProgressUpdate(String... values) {
+        protected void onProgressUpdate(Integer... values) {
             super.onProgressUpdate(values);
         }
 
+        @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
         @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            // 重新获取Actiity的强引用，并且判断是否存活
+            Activity activity = mWeakActivity.get();
+            if (activity == null || activity.isFinishing() || activity.isDestroyed()) {
+                // activity死亡了，不再做任何的事情
+                return;
+            }
+            // The activity is still valid, do main-thread stuff here
+
         }
     }
 }
