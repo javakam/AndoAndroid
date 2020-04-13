76da6bb86948fc547c43d6eff944e7969489baae
javakam <1772664927@qq.com>
2020-04-13 16:34:21 +0800

    支持Android 10

--- app/src/main/java/com/improve/modules/ui_hencoder/SportsView.java
+++ app/src/main/java/com/improve/modules/ui_hencoder/SportsView.java
@@ -1,15 +1,16 @@
 package com.improve.modules.ui_hencoder;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.Typeface;
-
-import androidx.annotation.Nullable;
-
+import android.os.Build;
+import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.view.View;
 
 
@@ -17,6 +18,7 @@ import android.view.View;
 // video https://ke.qq.com/webcourse/index.html#cid=334710&term_id=100397234&taid=2798467546422134&vid=5285890786251677089
 
 public class SportsView extends View {
+
     private static final float RING_WIDTH = dp2px(20);
     private static final float RADIUS = dp2px(150);
     private static final int CIRCLE_COLOR = Color.parseColor("#90A4AE");
@@ -50,7 +52,9 @@ public class SportsView extends View {
         // 绘制进度条
         paint.setColor(HIGHLIGHT_COLOR);
         paint.setStrokeCap(Paint.Cap.ROUND);
-        canvas.drawArc(getWidth() / 2 - RADIUS, getHeight() / 2 - RADIUS, getWidth() / 2 + RADIUS, getHeight() / 2 + RADIUS, -90, 225, false, paint);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            canvas.drawArc(getWidth() / 2 - RADIUS, getHeight() / 2 - RADIUS, getWidth() / 2 + RADIUS, getHeight() / 2 + RADIUS, -90, 225, false, paint);
+        }
 
         // 绘制文字
         paint.setTextSize(dp2px(100));
@@ -61,7 +65,7 @@ public class SportsView extends View {
         canvas.drawText("abab", getWidth() / 2, getHeight() / 2 - offset, paint);
     }
 
-    private float dp2px(float dp) {
+    private static float dp2px(float dp) {
         return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, Resources.getSystem().getDisplayMetrics());
     }
 }
